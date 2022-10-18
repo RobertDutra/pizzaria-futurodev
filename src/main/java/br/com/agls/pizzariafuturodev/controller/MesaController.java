7a3a960b36fa46d2a5be4615a4b752a46a52df3a
Robert Dutra <robertdutra10@hotmail.com>
2022-10-17 21:26:21 -0300

    Merge com master

--- src/main/java/br/com/agls/pizzariafuturodev/controller/MesaController.java
+++ src/main/java/br/com/agls/pizzariafuturodev/controller/MesaController.java
@@ -6,10 +6,9 @@ import br.com.agls.pizzariafuturodev.service.interfaces.MesaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/mesas")
@@ -23,4 +22,8 @@ public class MesaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.mesaServiceImpl.salvar(mesa));
     }
 
+    @GetMapping()
+    public ResponseEntity<List<Mesa>> lista(){
+        return ResponseEntity.ok().body(this.mesaServiceImpl.Listar());
+    }
 }
